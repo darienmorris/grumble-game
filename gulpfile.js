c76b1985127d0cf2ef9a9b1f9df6741745799422
Erik Krieg <erik.c.krieg@gmail.com>
2015-08-16 12:04:00 -0400

    Replaced server.js with gulp-connect and set it up in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
+var gulp = require('gulp'),
+  	connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+	connect.server({
+		livereload: true
+	});
 });
+ 
+gulp.task('html', function () {
+	gulp.src('*.html')
+		.pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+ 	gulp.watch(['*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
